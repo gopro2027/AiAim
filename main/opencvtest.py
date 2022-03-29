e7b8bbbb91fe52b1bb304836be217bdb5bd035f9
gopro2027 <gopro2027@gmail.com>
2022-03-29 00:11:54 -0500

    first test

--- main/opencvtest.py
+++ main/opencvtest.py
@@ -5,46 +5,55 @@
 import cv2
 from matplotlib import pyplot as plt
 import os, random
-  
-# Opening image
-imgpath = "img/rand/"+random.choice(os.listdir("img/rand/"))
-img = cv2.imread(imgpath)
 
-print("File chosen! " + imgpath)
-  
-# OpenCV opens images as BRG 
-# but we want it as RGB We'll 
-# also need a grayscale version
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
   
   
 # Use minSize because for not 
 # bothering with extra-small 
 # dots that would look like STOP signs
 stop_data = cv2.CascadeClassifier('misc/opencv/bin/cascade/cascade.xml')
+
+video_name = '2022-03-28_22-24-34.mp4'
+cap = cv2.VideoCapture('img/raw/'+video_name)
+if (cap.isOpened() == False):
+    print("Error opening video stream or file")
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+
+    if ret == True:
+        # OpenCV opens images as BRG 
+        # but we want it as RGB We'll 
+        # also need a grayscale version
+        img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
   
-found = stop_data.detectMultiScale(img_gray, 
-                                   minSize =(24, 24))
+        found = stop_data.detectMultiScale(img_gray, 
+                                   minSize =(20, 20))
   
-# Don't do anything if there's 
-# no sign
-amount_found = len(found)
+        # Don't do anything if there's 
+        # no sign
+        amount_found = len(found)
   
-if amount_found != 0:
+        if amount_found != 0:
       
-    # There may be more than one
-    # sign in the image
-    for (x, y, width, height) in found:
+            # There may be more than one
+            # sign in the image
+            for (x, y, width, height) in found:
           
-        # We draw a green rectangle around
-        # every recognized sign
-        cv2.rectangle(img_rgb, (x, y), 
+                # We draw a green rectangle around
+                # every recognized sign
+                cv2.rectangle(img_rgb, (x, y), 
                       (x + height, y + width), 
                       (0, 255, 0), 1)
-          
-# Creates the environment of 
-# the picture and shows it
-plt.subplot(1, 1, 1)
-plt.imshow(img_rgb)
-plt.show()
+
+        cv2.imshow('Unprocessed', img_rgb)
+
+        key = cv2.waitKey(1)
+        if (key == ord('q')):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
